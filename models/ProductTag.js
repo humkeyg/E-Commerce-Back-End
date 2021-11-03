23589287d1024eaa50d91db144a303229ab9cf4a
humkeyg <87458845+humkeyg@users.noreply.github.com>
2021-11-02 23:33:25 -0700

    First commit
    Changed file structure, also includes Associations and Model changes

---
+++ models/ProductTag.js
@@ -0,0 +1,34 @@
+const { Model, DataTypes } = require('sequelize');
+
+const sequelize = require('../config/connection');
+
+class ProductTag extends Model {}
+
+ProductTag.init(
+  {
+    // define columns\
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    productId: {
+      type: DataTypes.INTEGER
+      //need to refence product model id
+    },
+    tagId: {
+      type: DataTypes.INTEGER
+      //need to reference tag model id
+    }
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'product_tag',
+  }
+);
+
+module.exports = ProductTag;
